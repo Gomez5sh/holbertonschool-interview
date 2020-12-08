ddc94f6e355429926007e8fc6f25218ebcd4300f
g5sh <eagomez2387@gmail.com>
2020-12-08 18:31:21 -0500

    [ADD]: 0. Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    """Main funtion"""
+    for key in range(1, len(boxes) - 1):
+
+        boole = False
+
+        for position in range(len(boxes)):
+
+            boole = (key in boxes[position] and key != position)
+
+            if boole is True:
+                break
+
+        if boole is False:
+            return boole
+
+    return True
