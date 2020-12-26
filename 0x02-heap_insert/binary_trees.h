0e999f7af9f7dd554f8df65deab33e81253663c2
g5sh <eagomez2387@gmail.com>
2020-12-25 23:15:57 -0500

    [ADD]: Head project

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -1,3 +1,9 @@
+#ifndef _HOLBERTON_H
+#define _HOLBERTON_H
+
+#include <stddef.h>
+#include <stdio.h>
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -8,8 +14,15 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+#endif
