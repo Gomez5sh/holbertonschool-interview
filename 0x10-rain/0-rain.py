6af1a2e3be716d813c20afe540450cb09b84379b
gomez5sh <emmanuel.gomez@vertebrasoluciones.com>
2021-08-18 06:31:40 -0500

    [ADD]: Add task

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+
+
 """
 Module for Rain Task
 """
 
 
+
 def rain(walls):
-    """Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map
-    , calculate how many square units of water will be retained after it rains. Prototype:
-    * def rain(walls)
-    * walls is a list of non-negative integers.
-    * Return: Integer indicating total amount of rainwater retained.
-    * Assume that the ends of the list (before index 0 and after index walls[-1]) are not walls, meaning they will not retain water.
-    * If the list is empty return 0.
+    """
+    Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains
     """
     if not walls or not isinstance(walls, list):
         return 0
